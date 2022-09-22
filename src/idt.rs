fd8eb5e68f56c9a2455a21c6f060e105bd21a720
Dylan Turner <dylantdmt@gmail.com>
2022-09-21 21:20:35 -0500

    idt.rs: added more complex key handler

--- src/idt.rs
+++ src/idt.rs
@@ -12,7 +12,7 @@ use crate::{
     io::{
         inb, outb, remap_pic
     }, terminal::{
-        Color, print_char
+        Color, print_char, print_str, backspace
     }
 };
 
@@ -49,19 +49,82 @@ pub struct IdtDescriptor {
     base: u64
 }
 
-// Keyboard scan codes
-const SCANCODE_TABLE: [char; 58] = [
-    '\0', '\0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\0',
-    '\0', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\0', '\0',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',  ';', '\'', '`', '\0', '\\',
-    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '\0', '*', '\0', ' '
+// Keyboard scan codes; TODO: Move to user space
+const SCANCODE_TABLE: [char; 88] = [
+    '\0', // Nothing
+    '\0', // Escape - 0x01
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+    '\0', // Backspace - 0x0E
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+    '\0', // Left control - 0x1D
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',  ';', '\'', '`',
+    '\0', // Left shift - 0x2A
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '.', '/',
+    '\0', // Right shift - 0x36
+    '*',
+    '\0', // Left alt - 0x38
+    ' ',
+    '\0', // Caps lock - 0x3A
+    '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', // F1 (0x3B) -> F10 (0x44)
+    '\0', // Num lock - 0x45
+    '\0', // Scroll lock - 0x46
+
+    // Keypad numerals. Default to num lock on
+    '7', '8', '9', '-',
+    '4', '5', '6', '+',
+    '1', '2', '3', '0', '.',
+    
+    '\0', '\0', '\0', // 0x54,55,56 - Nothing!
+    '\0', '\0' // 0x57, 0x58 - F11, F12
+    
+    // Everything else up to 0x8X (releases) is nothing
+    // Then later there's more special funcs
 ];
 
-// Keyboard handler
+// TODO: Move to user space
+fn keyboard_handler(code: u8, c: char, is_rel: bool) {
+    if c != '\0' && !is_rel {
+        print_char(c, Color::Black, Color::White);    
+    } else {
+        match code {
+            0x01 => print_str("<ESC>", Color::Blue, Color::White),
+            0x0E => backspace(),
+            0x1D => print_str("<LCTRL>", Color::Blue, Color::White),
+            0x2A => print_str("<LSHIFT>", Color::Blue, Color::White),
+            0x36 => print_str("<RSHIFT>", Color::Blue, Color::White),
+            0x38 => print_str("<LALT>", Color::Blue, Color::White),
+            0x3A => print_str("<CPSLK>", Color::Blue, Color::White),
+            0x3B => print_str("<F1>", Color::Blue, Color::White),
+            0x3C => print_str("<F2>", Color::Blue, Color::White),
+            0x3D => print_str("<F3>", Color::Blue, Color::White),
+            0x3E => print_str("<F4>", Color::Blue, Color::White),
+            0x3F => print_str("<F5>", Color::Blue, Color::White),
+            0x40 => print_str("<F6>", Color::Blue, Color::White),
+            0x41 => print_str("<F7>", Color::Blue, Color::White),
+            0x42 => print_str("<F8>", Color::Blue, Color::White),
+            0x43 => print_str("<F9>", Color::Blue, Color::White),
+            0x44 => print_str("<F10>", Color::Blue, Color::White),
+            0x45 => print_str("<NMLK>", Color::Blue, Color::White),
+            0x46 => print_str("<SCRLK>", Color::Blue, Color::White),
+            0x57 => print_str("<F11>", Color::Blue, Color::White),
+            0x48 => print_str("<F12>", Color::Blue, Color::White),
+            _ => {} // Not handled
+        }
+    }
+}
+
+// Keyboard interrupt handler
 #[no_mangle]
 pub extern "C" fn isr1_handler() {
     let code = inb(0x60);
-    print_char(SCANCODE_TABLE[code as usize], Color::Black, Color::White);
+    let mut c = '\0';
+    if (code as usize) < SCANCODE_TABLE.len() {
+        c = SCANCODE_TABLE[code as usize];
+    } else if (code & 0x80) > 0 && ((code & (!0x80)) as usize) < SCANCODE_TABLE.len() {
+        c = SCANCODE_TABLE[(code & (!0x80)) as usize];
+    }
+    let rel = (code | 0x80) > 0;
+    keyboard_handler(code, c, rel); // TODO: call user land code somehow
 
     outb(0x20, 0x20);
     outb(0xA0, 0x20);
